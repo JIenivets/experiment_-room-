7b0d2be34de2321c7e13ffd07827b02dc4fb0758
Sergei Kochkyrkin <sergeikochkurkin@yandex.ru>
2021-01-23 21:15:13 +0300

    Save 8 General:     ⚫ Added restart Enemies:     Boss:         ⚫ Added spikes         Spikes:             ⚫ Spikes can spawn             ⚫ Spikes can move and give damage

--- mario — копия.py
+++ mario — копия.py
@@ -9,21 +9,30 @@ FPS = 50
 WIDTH = 500
 HEIGHT = 500
 STEP = 1
+SPIKES_DAMAGE = 15
+SPIKES_SPEED = 2
 KILL_SCORE = 1
 KILL_BOSS_SCORE = 10
+TIMER_SET = pygame.USEREVENT + 1
+EMEMIES_MOVE = pygame.USEREVENT + 2
 ENEMY_LIST = ['\Slime', '\AngryPig', '\Bat']
 FRUITS_LIST = ['apple', 'bananas', 'cherries', 'kiwi',
                'melon', 'orange', 'pineapple', 'strawberry']
+SPIKES = [('spik1'), ('spik2'), ('spik3'),
+          ('spik4'), ('spik5')]
 
 pygame.init()
 size = WIDTH, HEIGHT
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+start_x = None
+start_y = None
 player_direction = 'right'
 kills_number = 49
 spawn_boss = False
 damage_take = True
 over = False
+spikes_move = False
 # основной персонаж
 player = None
 boss = None
@@ -36,6 +45,7 @@ fireBoll_group = pygame.sprite.Group()
 countKills_group = pygame.sprite.Group()
 boss_enemy_group = pygame.sprite.Group()
 fruit_group = pygame.sprite.Group()
+spikes_group = pygame.sprite.Group()
 
 
 
@@ -76,6 +86,7 @@ tile_images = {
     'grass': load_image('sprites', 'Green.png'),
     'spawn_plase': load_image('sprites', 'Green.png'),
     'spawn_boss_plase': load_image('sprites', 'Green.png'),
+    'spawn_player_plase': load_image('sprites', 'Green.png'),
     'wall': load_image('sprites', 'wall.png')
 
 }
@@ -85,6 +96,7 @@ countKills_image = load_image('sprites\chisla', '0.png', colorkey=-1)
 score_image = pygame.transform.scale(load_image('sprites', 'score.png', colorkey=-1), (84, 16))
 heart = pygame.transform.scale(load_image('sprites', 'heart.png', colorkey=-1), (33, 33))
 gameover = pygame.transform.scale(load_image('sprites', 'game over.png', colorkey=-1), (280, 180))
+restart = pygame.transform.scale(load_image('sprites', 'restart.png', colorkey=-1), (370, 27))
 
 tile_width = tile_height = 64
 
@@ -188,9 +200,10 @@ class EnemysBoss(pygame.sprite.Sprite):
                                             (123, 78))
         self.mack = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(pos_x, pos_y)
+        pygame.time.set_timer(TIMER_SET, 500)
 
     def updete(self):
-        global kills_number, spawn_boss, score, spawn_fruits
+        global kills_number, spawn_boss, score, spawn_fruits, over
         if self.health <= 0:
             spawn_fruits(boss.rect.x, boss.rect.y, boss.rect.w, boss.rect.h)
             self.kill()
@@ -211,11 +224,66 @@ class EnemysBoss(pygame.sprite.Sprite):
                                                 (123, 78))
             self.direction = 'r'
 
+    def spawn_spikes(self):
+        global boss
+        for s in SPIKES:
+            if s == 'spik1':
+                BossSpikes(self.rect.x, self.rect.y + (self.rect.h - 27), s, (18, 21))
+            elif s == 'spik2':
+                BossSpikes(self.rect.x + 15, self.rect.y + 15, s, (18, 18))
+            elif s == 'spik3':
+                BossSpikes(self.rect.x + 48, self.rect.y + 20, s, (21, 21))
+            elif s == 'spik4':
+                BossSpikes(self.rect.x + (self.rect.w - 36), self.rect.y + 16, s, (21, 21))
+            elif s == 'spik5':
+                BossSpikes(self.rect.x + (self.rect.w - 20), self.rect.y + (self.rect.h - 27), s, (21, 21))
+
     def boss_health(self):
         pygame.draw.rect(screen, (0, 0, 0), (self.rect.x + 10, self.rect.y - 16,
                                              self.health + 1, 11), 3)
         pygame.draw.rect(screen, (255, 0, 0), (self.rect.x + 11, self.rect.y - 15,
                                                self.health, 10))
+
+    def move(self):
+        self.rect.x += randint(-5, 5)
+        self.rect.y += randint(-5, 5)
+
+
+class BossSpikes(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, who, size):
+        super().__init__(spikes_group, all_sprites)
+        self.who = who
+        self.image = pygame.transform.scale(load_image('sprites\Enemies\Bosses', self.who + '.png', colorkey=-1),
+                                            size)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect().move(pos_x, pos_y)
+
+    def move(self):
+        if self.who == 'spik1':
+            self.rect.x -= SPIKES_SPEED
+        elif self.who == 'spik2':
+            self.rect.x -= SPIKES_SPEED
+            self.rect.y -= SPIKES_SPEED
+        elif self.who == 'spik3':
+            self.rect.y -= SPIKES_SPEED
+        elif self.who == 'spik4':
+            self.rect.x += SPIKES_SPEED
+            self.rect.y -= SPIKES_SPEED
+        elif self.who == 'spik5':
+            self.rect.x += SPIKES_SPEED
+        self.check()
+
+    def check(self):
+        global player, spikes_move
+        for t in tiles_group:
+            if t.type == 'wall' and collide_rect(self, t):
+                self.kill()
+                spikes_move = False
+            elif collide_rect(self, player):
+                spikes_move = False
+                self.kill()
+                player.health -= SPIKES_DAMAGE
+                break
 
 
 class Enemys(pygame.sprite.Sprite):
@@ -227,6 +295,7 @@ class Enemys(pygame.sprite.Sprite):
         self.rect = self.image.get_rect().move(pos_x, pos_y)
 
     def update(self):
+        global player
         for en in enemy_group:
             if player.rect.x < en.rect.x and en.rect.x - player.rect.x <= 20 and en.direction == 'r':
                 en.image = load_image('sprites\Enemies' + en.who, 'stay_l.png', colorkey=-1)
@@ -235,6 +304,9 @@ class Enemys(pygame.sprite.Sprite):
                     and en.direction == 'l':
                 en.image = load_image('sprites\Enemies' + en.who, 'stay_r.png', colorkey=-1)
                 en.direction = 'r'
+
+    def move(self):
+        pass
 
 
 def spawn_enemis():
@@ -321,10 +393,9 @@ class Player(pygame.sprite.Sprite):
     def player_health(self):
         global over
         if self.health > 0:
-            pygame.draw.rect(screen, (0, 0, 0), (38, 7, self.health + 1, 16), 3)
+            pygame.draw.rect(screen, (0, 0, 0), (38, 7, self.health + 2, 17), 2)
             pygame.draw.rect(screen, (255, 0, 0), (39, 8, self.health, 15))
         else:
-            player.kill()
             over = True
 
     def take_damage(self, take, renge):
@@ -385,6 +456,7 @@ class FireBoll(pygame.sprite.Sprite):
 
 
 def generate_level(level):
+    global start_x, start_y
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
@@ -397,8 +469,9 @@ def generate_level(level):
             elif level[y][x] == 'B':
                 Tile('spawn_boss_plase', x, y)
             elif level[y][x] == '@':
-                Tile('grass', x, y)
+                Tile('spawn_player_plase', x, y)
                 new_player = Player(x, y)
+                start_x, start_y = x, y
                 # new_player = AnimatedSprite(load_image('sprites\Main Characters\Virtual Guy', 'Idle (32x32).png', colorkey=-1), 11, 1, x, y)
     # вернем игрока, а также размер поля в клетках
     return new_player, x, y
@@ -407,8 +480,8 @@ def generate_level(level):
 def game_draw():
     global kills_number, spawn_boss, boss
     tiles_group.draw(screen)
-    enemy_group.draw(screen)
     fireBoll_group.draw(screen)
+    enemy_group.draw(screen)
     if len(fruit_group) > 0:
         fruit_group.draw(screen)
     player_group.draw(screen)
@@ -417,9 +490,15 @@ def game_draw():
             if pos.type == 'spawn_boss_plase':
                 boss = EnemysBoss(pos.rect.x + randint(-30, 30), pos.rect.y + randint(-40, 40))
         spawn_boss = True
+    spikes_group.draw(screen)
     if spawn_boss:
         kills_number = 49
+        if len(spikes_group) == 0:
+            boss.spawn_spikes()
+        spikes_group.draw(screen)
         boss_enemy_group.draw(screen)
+        for s in spikes_group:
+            s.move()
         boss.updete()
         boss.rotation()
     screen.blit(heart, (0, 0))
@@ -433,6 +512,32 @@ def game_draw():
     player.player_health()
     fireBoll_group.update()
     enemy_group.update()
+
+
+def restart_game():
+    global over, player, score, kills_number, kills, boss, spawn_boss
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_f:
+                screen.fill((33, 31, 100))
+                pygame.mouse.set_visible(True)
+                player.health = 100
+                for p in tiles_group:
+                    if p.type == 'spawn_player_plase':
+                        player.rect.x, player.rect.y = p.rect.x, p.rect.y
+                        break
+                kills_number = 0
+                kills.load_number(kills_number)
+                score.score = 0
+                if spawn_boss:
+                    boss.kill()
+                    spawn_boss = False
+                for s in spikes_group:
+                    s.kill()
+                spikes_group.empty()
+                fruit_group.empty()
+                enemy_group.empty()
+                over = False
 
 
 player, level_x, level_y = generate_level(load_level('map.txt'))
@@ -455,7 +560,7 @@ while running:
             # при закрытии окна
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and not over:
             beforex = player.rect.x
             beforey = player.rect.y
             if event.key == pygame.K_LEFT or event.key == pygame.K_a:
@@ -470,16 +575,23 @@ while running:
                 player.rect.y -= STEP
             if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                 player.rect.y += STEP
+            if event.key == pygame.K_r:
+                kills_number = 49
             player.updete()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if len(fireBoll_group) < 5:
                 FireBoll(event.pos)
+        if spawn_boss:
+            if event.type == TIMER_SET:
+                spikes_move = True
     screen.fill((33, 31, 100))
     if not over:
             game_draw()
     else:
-            screen.blit(gameover, (110, 160))
+            screen.blit(gameover, (110, 133))
+            screen.blit(restart, (65, 343))
             pygame.mouse.set_visible(False)
+            restart_game()
     camera.update(player)
     # обновляем положение всех спрайтов
     for sprite in all_sprites:
